eec83afacab5c13046bc11a8fed8b57cb6809445
Marcelot <marcelojtelles@gmail.com>
2019-04-06 20:10:56 -0300

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dg1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            this.txtConsulta = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conecta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Inserir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(69, 126);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nome";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Figura";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(35, 157);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dg1
+            // 
+            this.dg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg1.Location = new System.Drawing.Point(35, 322);
+            this.dg1.Name = "dg1";
+            this.dg1.Size = new System.Drawing.Size(514, 171);
+            this.dg1.TabIndex = 9;
+            this.dg1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dg1_CellClick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(116, 264);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Lista 2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Location = new System.Drawing.Point(462, 281);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(75, 23);
+            this.btnConsulta.TabIndex = 11;
+            this.btnConsulta.Text = "Consulta";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            this.btnConsulta.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // txtConsulta
+            // 
+            this.txtConsulta.Location = new System.Drawing.Point(295, 283);
+            this.txtConsulta.Name = "txtConsulta";
+            this.txtConsulta.Size = new System.Drawing.Size(145, 20);
+            this.txtConsulta.TabIndex = 12;
+            this.txtConsulta.TextChanged += new System.EventHandler(this.txtConsulta_TextChanged);
+            this.txtConsulta.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtConsulta_KeyUp);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(35, 293);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Insert 100";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(256, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Alterar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_3);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(453, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(96, 23);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "Cria banco";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(453, 41);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(96, 23);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "Criar tabela";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 516);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.txtConsulta);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dg1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dg1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnConsulta;
+        private System.Windows.Forms.TextBox txtConsulta;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog file = null;
+        long tamanho = 0;
+        byte[] imagem;
+        string str;
+        MySqlCommand sqlcmd;
+        MySqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = conectar("banco001");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        protected MySqlConnection conectar(string onde)
+        {
+            MySqlConnection conn;
+            string Configuracao = "server=localhost; userid=root; database="+onde;
+            conn = new MySqlConnection(Configuracao);
+            try
+            {
+                conn.Open();
+                //MessageBox.Show("Conectado");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro" + ex.Message);
+
+            }
+            return conn;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Carregar();
+        }
+
+        protected void Carregar()
+        {
+            try
+            {
+                openFileDialog1.ShowDialog(this);
+                str = this.openFileDialog1.FileName;
+                //MessageBox.Show("." + str);
+
+                if (string.IsNullOrEmpty(str))
+                {
+                    return;
+                }
+
+                this.pictureBox1.Image = Image.FromFile(str);
+                FileInfo arq = new FileInfo(str);
+                tamanho = arq.Length;
+                FileStream fs = new FileStream(str, FileMode.Open, FileAccess.Read, FileShare.Read);
+                imagem = new byte[Convert.ToInt32(tamanho + 1)];
+                int byteRead = fs.Read(imagem, 0, Convert.ToInt32(this.tamanho));
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("erro na carregar" + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                conn = conectar("banco001");
+                string comando;
+
+                comando = "insert into funcionario(nome,figura)" +
+                    " values (@nome,@figura)";
+                sqlcmd = new MySqlCommand(comando, conn);
+
+
+                sqlcmd.Parameters.AddWithValue("@nome", textBox2.Text);
+                sqlcmd.Parameters.AddWithValue("@figura", imagem);
+
+                MySqlDataReader insert = sqlcmd.ExecuteReader();
+
+                if (!insert.Read())
+                {
+                    MessageBox.Show("Paciente cadastrado com sucesso!");
+                }
+
+                else
+                {
+                    MessageBox.Show("Cadastro inválido");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(sqlcmd.ToString() + " " + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = conectar("banco001");
+                string comando;
+                int i = 0;
+
+                comando = "select * from funcionario";
+                sqlcmd = new MySqlCommand(comando, conn);
+
+
+                MySqlDataReader consulta = sqlcmd.ExecuteReader();
+
+                while (consulta.Read())
+                {
+                    dg1.Rows.Add();
+
+                    dg1.Rows[i].Cells["id"].Value = Convert.ToString(consulta["id"]);
+
+                    dg1.Rows[i].Cells["nome"].Value = Convert.ToString(consulta["nome"]);
+
+
+                    i++;
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(sqlcmd.ToString() + " " + ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            conn = conectar("banco001");
+
+
+            string sql = "Select id,nome from funcionario;";
+
+            MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
+
+            DataSet dataset_eaquiquevai = new DataSet();
+            da.Fill(dataset_eaquiquevai, "funcionario");
+
+            dg1.DataSource = dataset_eaquiquevai;
+            dg1.DataMember = "funcionario";
+        }
+
+        private void dg1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = this.dg1.Rows[e.RowIndex];
+            string id = row.Cells[0].Value.ToString();
+
+            conn = conectar("banco001");
+            string comando;
+            int i = 0;
+
+            comando = "select * from funcionario WHERE id="+id;
+            sqlcmd = new MySqlCommand(comando, conn);
+
+
+            MySqlDataReader consulta = sqlcmd.ExecuteReader();
+
+            while (consulta.Read())
+            {
+                Console.WriteLine(Convert.ToString(consulta["id"]));
+                textBox1.Text = Convert.ToString(consulta["id"]);
+                textBox2.Text = Convert.ToString(consulta["nome"]);
+            }
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            conn = conectar("banco001");
+
+
+            string sql = "Select id,nome from funcionario WHERE nome LIKE '%" + txtConsulta.Text + "%';";
+
+            MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
+
+            DataSet dataset_eaquiquevai = new DataSet();
+            da.Fill(dataset_eaquiquevai, "funcionario");
+
+            dg1.DataSource = dataset_eaquiquevai;
+            dg1.DataMember = "funcionario";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string comando;
+
+                for (int i = 0; i < 100; i++)
+                {
+                    conn = conectar("banco001");
+                    comando = "insert into funcionario(nome,figura)" +
+                    " values (@nome,@figura)";
+                    sqlcmd = new MySqlCommand(comando, conn);
+                    sqlcmd.Parameters.AddWithValue("@nome", textBox2.Text+" "+i);
+                    sqlcmd.Parameters.AddWithValue("@figura", imagem);
+                    MySqlDataReader insert = sqlcmd.ExecuteReader();
+                    conn.Close();
+                }
+
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(sqlcmd.ToString() + " " + ex.Message);
+            }
+        }
+
+        private void txtConsulta_KeyUp(object sender, KeyEventArgs e)
+        {
+            conn = conectar("banco001");
+
+
+            string sql = "Select id,nome from funcionario WHERE nome LIKE '%" + txtConsulta.Text + "%';";
+
+            MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
+
+            DataSet dataset_eaquiquevai = new DataSet();
+            da.Fill(dataset_eaquiquevai, "funcionario");
+
+            dg1.DataSource = dataset_eaquiquevai;
+            dg1.DataMember = "funcionario";
+            conn.Close();
+        }
+
+        private void button4_Click_2(object sender, EventArgs e)
+        {
+            conn.Close();
+            
+        }
+
+        private void txtConsulta_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click_3(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = conectar("banco001");
+                string comando;
+
+                comando = "UPDATE funcionario SET nome=@nome,figura=@figura WHERE id="+textBox1.Text;
+                sqlcmd = new MySqlCommand(comando, conn);
+
+
+                sqlcmd.Parameters.AddWithValue("@nome", textBox2.Text);
+                sqlcmd.Parameters.AddWithValue("@figura", imagem);
+
+                MySqlDataReader insert = sqlcmd.ExecuteReader();
+
+                if (!insert.Read())
+                {
+                    MessageBox.Show("Paciente alterado com sucesso!");
+                }
+
+                else
+                {
+                    MessageBox.Show("Cadastro inválido");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(sqlcmd.ToString() + " " + ex.Message);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = conectar("mysql");
+                string comando;
+                comando = "CREATE DATABASE banco001";
+                sqlcmd = new MySqlCommand(comando, conn);
+                sqlcmd.ExecuteNonQuery();
+                MessageBox.Show("Banco criado" );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro " + ex.Message);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = conectar("banco001");
+                string comando;
+                comando = "CREATE TABLE IF NOT EXISTS `funcionario` ("
+                          +"`id` int(11) NOT NULL AUTO_INCREMENT,"
+                          +"`nome` varchar(50) NOT NULL,"
+                          +"`figura` mediumblob NOT NULL,"
+                          +"`dataGravacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+                          +"PRIMARY KEY (`id`)"
+                          +") ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
+                sqlcmd = new MySqlCommand(comando, conn);
+                sqlcmd.ExecuteNonQuery();
+                MessageBox.Show("Tabela criada");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro " + ex.Message);
+            }
+        }
+    }
+}
